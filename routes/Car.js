f708b878674ab35a64136a0aaeb6c9e1c73c4bc4
onyou_exe <mick.pongsakorn@gmail.com>
2026-02-27 11:18:16 +0700

    fix routes hospital to Car

--- routes/Car.js
+++ routes/Car.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getHospitals,getHospital,createHospital,updateHospital,deleteHospital} = require('../controllers/hospitals');
+const {getHospitals,getHospital,createHospital,updateHospital,deleteHospital} = require('../controllers/Car');
 
 const appointmentRouter=require('./appointments');
 
@@ -9,7 +9,7 @@ const {protect,authorize} = require('../middleware/auth');
 
 router.use('/:hospitalId/appointments/',appointmentRouter);
 
-router.route('/').get(getHospitals).post(protect,authorize('admin'),createHospital);
+router.route('/').get(getCars).post(protect,authorize('admin'),createHospital);
 router.route('/:id').get(getHospital).put(protect,authorize('admin'),updateHospital).delete(protect,authorize('admin'),deleteHospital);
 
 /**
